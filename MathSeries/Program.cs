61a903697d4d20908d62d4d7cccb8efa822ac118
Andrey Valchuk <immigrationimmigrant@gmail.com>
2021-02-28 20:21:33 +0300

    Add project files.

---
+++ MathSeries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MathSeries
+{
+    class Program
+    {
+        public static double CalculateSeries(double xValue, uint startPoint, uint endPoint, Func<double, uint, double> formula)
+        {
+            if (startPoint >= endPoint)
+            {
+                throw new ArgumentException("End point value must be greater than start point value.");
+            }
+
+            double result = 0;
+
+            for (uint i = startPoint; i <= endPoint; i++)
+            {
+                result += formula(xValue, i);
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите k и x: ");
+            string[] input = Console.ReadLine().Split();
+
+            uint k = UInt32.Parse(input[0]);
+            double x = Double.Parse(input[1]);
+
+            double result = CalculateSeries(x, 0, k, (xValue, n) =>
+            {
+                double factorial = 1;
+
+                for (uint i = 2; i <= (2 * n + 1); i++)
+                {
+                    factorial *= i;
+                }
+
+                return Math.Pow(xValue, 2 * n + 1) / factorial;
+            });
+
+            Console.WriteLine($"Значение суммы: {result}");
+        }
+    }
+}
+
